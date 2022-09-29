a063c685a97e8319ab23662cc21e1d2b79a6d50b
elb136 <93697823+elb136@users.noreply.github.com>
2022-09-29 14:24:39 +0300

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors/Color_Sensor.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+import android.graphics.Color;
+
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import org.opencv.imgproc.Imgproc;
+
+public class Color_Sensor {
+    static final float[] hsv = new float[3];
+    static final float[] rgb = new float[3];
+    static NormalizedColorSensor colorSensor;
+    static public void init(HardwareMap hardwareMap){
+        colorSensor = hardwareMap.get(NormalizedColorSensor.class,"color");
+
+    }
+    public static float[] HSV(){
+        NormalizedRGBA colors = colorSensor.getNormalizedColors();
+        Color.colorToHSV(colors.toColor(), hsv);
+        return hsv;
+    }
+}
