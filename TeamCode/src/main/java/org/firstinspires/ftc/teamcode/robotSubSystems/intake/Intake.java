3069e12c955f918230674c61f0323be7f5bed4ba
iyar <iyar221c@gmail.com>
2022-10-06 15:46:23 +0300

    wrote init adn operate methods for intake subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/intake/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/intake/Intake.java
@@ -1,13 +1,30 @@
 package org.firstinspires.ftc.teamcode.robotSubSystems.intake;
 
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Intake {
 
-    public void init() {
+    public static final DcMotor motors[] = new DcMotor[2];
 
+    public void init(HardwareMap hardwareMap) {
+
+        motors[0] = hardwareMap.get(DcMotor.class, "IntakeR");
+        motors[1] = hardwareMap.get(DcMotor.class, "IntakeL");
+        //TODO: reverse correct motor according to robot
+
+        for (final DcMotor motor : motors) {
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
     }
 
     public void operate() {
+
+        for (final DcMotor motor : motors) {
+            motor.setPower(1);
+            //TODO: change power according to tests
+        }
 
     }
 
