83811131903a7eab54cd8927c96bb99e59faa386
elad 2 <eladbenaim@gmail.com>
2022-10-19 21:18:44 +0300

    started to work on the capturing method the detecting junction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ImageProc/OpenCv.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ImageProc/OpenCv.java
@@ -4,6 +4,7 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -13,37 +14,38 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 @Config
 @TeleOp(name = "Camera test")
-public class OpenCv extends OpMode {
+public class OpenCv extends LinearOpMode {
     OpenCvCamera camera;
     Pipeline pipeline = new Pipeline(telemetry);
+    FtcDashboard dashboard = FtcDashboard.getInstance();
+
     @Override
-    public void init() {
-        telemetry = new MultipleTelemetry(telemetry,FtcDashboard.getInstance().getTelemetry());
+    public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
         WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
-        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
-                camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
-                camera.setPipeline(pipeline);
-                FtcDashboard.getInstance().startCameraStream(camera, 60);
-                TelemetryPacket packet = new TelemetryPacket();
-                packet.put("place", pipeline.currentPos);
-                FtcDashboard.getInstance().sendTelemetryPacket(packet);
+                if(gamepad1.a) {
+                    camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+                    camera.setPipeline(pipeline);
+                    FtcDashboard.getInstance().startCameraStream(camera, 60);
+                    TelemetryPacket packet = new TelemetryPacket();
+                    //packet.put("place", pipeline.picCount);
+                    FtcDashboard.getInstance().sendTelemetryPacket(packet);
+                }
             }
+
             @Override
-            public void onError(int errorCode)
-            {
+            public void onError(int errorCode) {
+
             }
         });
 
-
+    }
     }
 
-    @Override
-    public void loop() {
-    }
 
-}
+
