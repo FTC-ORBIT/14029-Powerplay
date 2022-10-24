cc28f53d110565732f77a2f7c94b1a88d18950a0
bar2005 <nc14102005b@big.atid.org.il>
2022-10-24 10:53:08 +0300

    reWrite DistanceSensor class in order to adjust it to the new Distance sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors/OrbitDistSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors/OrbitDistSensor.java
@@ -1,18 +1,19 @@
 package org.firstinspires.ftc.teamcode.Sensors;
 
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class OrbitDistSensor {
 
-    private final ModernRoboticsI2cRangeSensor sensor;
+    private DistanceSensor distanceSensor;
 
     public OrbitDistSensor(HardwareMap hardwareMap) {
-        sensor = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "distanceSensor");
+      distanceSensor = hardwareMap.get(DistanceSensor.class, "distanceSensor")  ;
     }
 
-    public float getDistance(){return (float) sensor.getDistance(DistanceUnit.CM);}
+    public float getDistance(){return (float) distanceSensor.getDistance(DistanceUnit.CM);}
 
 }
