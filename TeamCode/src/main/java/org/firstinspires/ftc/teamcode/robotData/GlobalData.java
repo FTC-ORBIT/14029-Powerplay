183b01e15531791cb546f52f6b14f80d4bd8e880
bar2005 <nc14102005b@big.atid.org.il>
2022-11-01 16:13:00 +0200

    solved issues

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotData/GlobalData.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotData/GlobalData.java
@@ -4,8 +4,8 @@ import org.firstinspires.ftc.teamcode.robotSubSystems.RobotState;
 
 public class GlobalData {
     public static final boolean inComp = false;
-    public static boolean hasGamePiece;
-    public static boolean inAutonomous;
+    public static boolean hasGamePiece = false;
+    public static boolean inAutonomous = false;
     public static float currentTime = 0;
     public static float lastTime = 0;
     public static float deltaTime = 0;
