07792311d9c184272c6faac5788f042135898420
Birdoflames <drnmalka@gmail.com>
2022-11-02 16:34:35 +0200

    in process

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ImageProc/TestPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ImageProc/TestPipeline.java
@@ -14,6 +14,7 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.core.Core;
 
 
+import org.opencv.imgproc.Moments;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 import java.util.ArrayList;
@@ -39,97 +40,104 @@ public class TestPipeline extends OpenCvPipeline {
         Mat blur = new Mat();
         Mat hsv = new Mat();
         Mat mask = new Mat();
-        Mat output = new Mat();
+        Mat hierarchy = new Mat();
+
         //Filters:
         Imgproc.blur(input, blur, new Size(7,7));
         Imgproc.cvtColor(blur, hsv, Imgproc.COLOR_BGR2HSV);
+
+
         //Ranging the yellow objects:
         Scalar low = new Scalar(0,0,0);
         Scalar high = new Scalar(255,255,255);
         Core.inRange(hsv, low, high, mask);
-
-
-
-
-        Mat dilate = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(24, 24));
-        Mat erode = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(12, 12));
-
-        Imgproc.dilate(mask, output, dilate);
-        Imgproc.dilate(mask, output, dilate);
-
-        List<MatOfPoint> contours = new ArrayList<>();
-        Mat hierarchy = new Mat();
-
-        Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
-        if (hierarchy.size().height > 0 && hierarchy.size().width > 0)
-        {
-            // for each contour, display it in blue
-            for (int idx = 0; idx >= 0; idx = (int) hierarchy.get(0, idx)[0])
-            {
-                Imgproc.drawContours(input, contours, idx, new Scalar(0, 0, 0));
-                
-            }
-
-
-
-
-
-
-
-
-            Imgproc.cvtColor(input, hsv, Imgproc.COLOR_BGR2HSV);
-        Mat mask = new Mat();
-        Core.inRange(hsv, new Scalar(13, 85 ,103), new Scalar(77, 255, 255), mask);
-
-
-//        Size size = new Size(3, 3);
-//        Mat blur = new Mat();
-//        //Imgproc.blur(gray, blur, size);
-//        Mat cannyOut = new Mat();
-//        Imgproc.Canny(gray, cannyOut, 100, 200);
-//
-        List<MatOfPoint> contours = new ArrayList<>();
-        Mat hierarchy = new Mat();
-        Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-        MatOfPoint2f[] contoursMask = new MatOfPoint2f[contours.size()];
-        Rect[] boundRect = new Rect[contours.size()];
-        Point[] centers = new Point[contours.size()];
-        float[][] radius = new float[contours.size()][1];
-
-        for (int i=0; i < contours.size(); i++){
-            contoursMask[i] = new MatOfPoint2f();
-            boundRect[i] = Imgproc.boundingRect(new MatOfPoint(contoursMask[i].toArray()));
-            centers[i] = new Point();
-            //Imgproc.minEnclosingCircle(contoursMask[i], centers[i], radius[i]);
-
+        List <MatOfPoint> contours = new ArrayList<>();
+        Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_NONE);
+        hierarchy.release();
+        int cx = -1;
+        int cy = -1;
+        Rect areaRect = null;
+        boolean detected = false;
+        double minArea = 0; // The minimum pixels area of a junction from maximum +- 100 cm
+        double area = null;
+        for (MatOfPoint contour : contours){
+            double area = Imgproc.contourArea(contour);
+            areaRect = Imgproc.boundingRect(contour);
+            Moments M = Imgproc.moments(contour);
+            cx = (int)(M.m10/M.m00);
+            cy = (int)(M.m01/M.m00);
         }
-        Mat drawing = Mat.zeros(mask.size(), CvType.CV_8UC3);
-        List<MatOfPoint> contoursPolyList = new ArrayList<>(contoursMask.length);
-        for (MatOfPoint2f poly : contoursMask) {
-            contoursPolyList.add(new MatOfPoint(poly.toArray()));
+        Point pnt = new Point(cx, cy);
+        if (area > minArea){
+            detected = true;
         }
-        for (int i = 0; i < contours.size(); i++) {
-            Scalar color = new Scalar(255, 0, 0);
-            Imgproc.drawContours(drawing, contoursPolyList, i, color);
-            Imgproc.rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 2);
-            Imgproc.circle(drawing, centers[i], (int) radius[i][0], color, 2);
-        }
-
-
-
-
-//        //h: 13-77
-        //s: 85-255
-        //v: 103-255
-
-
-
-        // Finding the largest junction
-
-
-        //Imgproc.drawMarker(input, new Point (120, 160), new Scalar(255, 255, 255), Imgproc.MARKER_TILTED_CROSS, 20, 2, Imgproc.LINE_8);
-
+        Imgproc.rectangle(input, areaRect, new Scalar (255, 0, 0));
         return input;
+
+
+
+
+//        Mat dilate = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(24, 24));
+//        Mat erode = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(12, 12));
+//
+//        Imgproc.dilate(mask, output, dilate);
+//        Imgproc.dilate(mask, output, dilate);
+//
+//        List<MatOfPoint> contours = new ArrayList<>();
+//        Mat hierarchy = new Mat();
+//
+//        Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
+//        if (hierarchy.size().height > 0 && hierarchy.size().width > 0) {
+//            // for each contour, display it in blue
+//            for (int idx = 0; idx >= 0; idx = (int) hierarchy.get(0, idx)[0]) {
+//                Imgproc.drawContours(input, contours, idx, new Scalar(0, 0, 0));
+//
+//            }
+//
+//
+////        Size size = new Size(3, 3);
+////        Mat blur = new Mat();
+////        //Imgproc.blur(gray, blur, size);
+////        Mat cannyOut = new Mat();
+////        Imgproc.Canny(gray, cannyOut, 100, 200);
+////
+//
+//            Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+//            MatOfPoint2f[] contoursMask = new MatOfPoint2f[contours.size()];
+//            Rect[] boundRect = new Rect[contours.size()];
+//            Point[] centers = new Point[contours.size()];
+//            float[][] radius = new float[contours.size()][1];
+//
+//            for (int i = 0; i < contours.size(); i++) {
+//                contoursMask[i] = new MatOfPoint2f();
+//                boundRect[i] = Imgproc.boundingRect(new MatOfPoint(contoursMask[i].toArray()));
+//                centers[i] = new Point();
+//                //Imgproc.minEnclosingCircle(contoursMask[i], centers[i], radius[i]);
+//
+//            }
+//            Mat drawing = Mat.zeros(mask.size(), CvType.CV_8UC3);
+//            List<MatOfPoint> contoursPolyList = new ArrayList<>(contoursMask.length);
+//            for (MatOfPoint2f poly : contoursMask) {
+//                contoursPolyList.add(new MatOfPoint(poly.toArray()));
+//            }
+//            for (int i = 0; i < contours.size(); i++) {
+//                Scalar color = new Scalar(255, 0, 0);
+//                Imgproc.drawContours(drawing, contoursPolyList, i, color);
+//                Imgproc.rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 2);
+//                Imgproc.circle(drawing, centers[i], (int) radius[i][0], color, 2);
+//            }
+//
+//
+////        //h: 13-77
+//            //s: 85-255
+//            //v: 103-255
+//
+//
+//            // Finding the largest junction
+//
+//
+//            //Imgproc.drawMarker(input, new Point (120, 160), new Scalar(255, 255, 255), Imgproc.MARKER_TILTED_CROSS, 20, 2, Imgproc.LINE_8);
+//        }
 
     }
 }
