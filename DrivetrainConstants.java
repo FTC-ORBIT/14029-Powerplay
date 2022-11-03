9744f644cea452b951b4769a6f2952a48ab4771e
eilon328 <62439278+eilon328@users.noreply.github.com>
2022-11-03 14:54:16 +0200

    Add files via upload

---
+++ DrivetrainConstants.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.robotSubSystems.drivetrain;
+
+public class DrivetrainConstants {
+    public static final float trackWidth = 0.418f;
+    public final static float forward_offset = -0.2f;
+    public final static float deadWheelDiameter = 0.035f;
+    public final static float ticksPerRevolutionExternalEncoder = 8192;
+    public final static double wheelErrorRange = 0.5;
+    public final static int requiredTimeWithInactiveMotor =1500;
+    public final static double deltaTimeOfInactiveMotor =0.004;
+}
