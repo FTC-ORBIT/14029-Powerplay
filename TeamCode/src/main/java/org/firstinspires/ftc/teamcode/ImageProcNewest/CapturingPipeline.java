23ea7afecbd0971949882022e0f60bbcb56491af
Birdoflames <drnmalka@gmail.com>
2022-12-04 13:59:03 +0200

    rotatedRect works

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ImageProcNewest/CapturingPipeline.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.ImageProc;
+import com.acmerobotics.dashboard.config.Config;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.opencv.core.Scalar;
+import org.opencv.core.Point;
+
+@Config
+public class CapturingPipeline extends OpenCvPipeline {
+    CapturingPipeline(Telemetry telemetry) {
+        this.telemetry = telemetry;
+    }
+
+    Telemetry telemetry;
+    int picCount = 0;
+
+
+    @Override
+    public Mat processFrame(Mat inFrame) {
+
+        picCount = picCount+1;
+        String picFileName;
+        String picFilePath;
+
+        picFileName= Integer.toString(picCount) + ".bmp";
+        picFilePath = "/sdcard/FIRST/" + picFileName;
+        Imgcodecs.imwrite(picFilePath, inFrame);
+        Imgproc.putText(inFrame, picFileName, new Point(20, 20), 2, 1, new Scalar(255, 0 ,0));
+
+        return inFrame;
+
+    }
+
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ImageProcNewest/MainPipeline.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.ImageProc;
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class MainPipeline extends OpenCvPipeline {
+
+    @Override
+    public Mat processFrame(Mat input) {
+
+        return input;
+    }
+}
