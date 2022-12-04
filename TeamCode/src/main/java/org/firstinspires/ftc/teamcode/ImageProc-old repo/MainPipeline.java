422514ecdac3b38e25aed12a4f476997e6b2807b
Birdoflames <drnmalka@gmail.com>
2022-12-04 12:15:17 +0200

    for sdk

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ImageProc-old repo/MainPipeline.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.ImageProc;
+import org.firstinspires.ftc.teamcode.ImageProc.TestPipeline;
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class MainPipeline extends OpenCvPipeline {
+
+    @Override
+    public Mat processFrame(Mat input) {
+
+        return input;
+    }
+}
